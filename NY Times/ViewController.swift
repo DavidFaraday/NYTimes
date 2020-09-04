f3999f1773d7ed38789e0f9e12f89ba56b1f7158
DavidFaraday <iosdevschool@gmail.com>
2020-09-04 19:17:53 +0300

    Initial Commit

---
+++ NY Times/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NY Times
+//
+//  Created by David Kababyan on 04/09/2020.
+//  Copyright © 2020 David Kababyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
